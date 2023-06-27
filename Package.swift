6dfe6fc434c35b94659011d198e0cf79b3e84c91
Elvis <elvisnunez@me.com>
2023-06-27 13:28:18 +0200

    Add exclude

--- Package.swift
+++ Package.swift
@@ -13,10 +13,12 @@ let package = Package(
     targets: [
         .target(
             name: "Designable",
-            path: "Designable/Sources"),
+            path: "Designable/Sources",
+            exclude: ["Demo"]),
         .testTarget(
             name: "DesignableTests",
             dependencies: ["Designable"],
             path: "Designable/Tests"),
+            exclude: ["Demo"]),
     ]
 )
