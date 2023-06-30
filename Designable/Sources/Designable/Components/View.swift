a1cd3c4d7e03b339eee82e153687f7a17ed0551e
Elvis <elvisnunez@me.com>
2023-06-30 21:51:07 +0200

    rollback?

--- Designable/Sources/Designable/Components/View.swift
+++ Designable/Sources/Designable/Components/View.swift
@@ -1,12 +1,6 @@
 import UIKit
 
 open class View: UIView {
-    public init() {
-        super.init(frame: .zero)
-        baseSetup()
-        setup()
-    }
-
     public override init(frame: CGRect) {
         super.init(frame: frame)
         baseSetup()
