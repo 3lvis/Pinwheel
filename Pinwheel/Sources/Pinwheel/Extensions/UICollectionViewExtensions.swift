a94233b579c69379081c042b85e5aa495ca99686
Elvis <elvisnunez@me.com>
2023-07-04 13:05:28 +0200

    Add collection view extensions

---
+++ Pinwheel/Sources/Pinwheel/Extensions/UICollectionViewExtensions.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+extension UICollectionReusableView: Identifiable {}
+
+public extension UICollectionView {
+    func register(_ cellClass: UICollectionViewCell.Type) {
+        register(cellClass.self, forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+    }
+
+    func register(_ cellClass: UICollectionReusableView.Type, ofKind kind: String) {
+        register(cellClass.self, forSupplementaryViewOfKind: kind, withReuseIdentifier: cellClass.reuseIdentifier)
+    }
+
+    func registerNib(_ cellClass: UICollectionViewCell.Type, bundle: Bundle? = nil) {
+        register(UINib(nibName: String(describing: cellClass), bundle: bundle), forCellWithReuseIdentifier: cellClass.reuseIdentifier)
+    }
+
+    func dequeue<T>(_ cellClass: T.Type, for indexPath: IndexPath) -> T where T: UICollectionViewCell {
+        return dequeueReusableCell(withReuseIdentifier: cellClass.reuseIdentifier, for: indexPath) as! T
+    }
+
+    func dequeue<T>(_ reusableSupplementaryViewClass: T.Type, for indexPath: IndexPath, ofKind kind: String) -> T where T: UICollectionReusableView {
+        return dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: reusableSupplementaryViewClass.reuseIdentifier, for: indexPath) as! T
+    }
+}
