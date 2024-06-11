cafaf1b86025513b63ebcada81b58fb9e56b45b8
Elvis <elvisnunez@me.com>
2024-06-11 15:34:16 +0200

    Fix bug

--- Pinwheel/Sources/Pinwheel/Extensions/Layout.swift
+++ Pinwheel/Sources/Pinwheel/Extensions/Layout.swift
@@ -20,7 +20,7 @@ public extension UIView {
         guard let superview = superview else { return }
 
         NSLayoutConstraint.activate([
-            topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: insets.top),
+            topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor, constant: insets.top),
             leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: insets.leading),
             trailingAnchor.constraint(equalTo: superview.trailingAnchor, constant: -insets.trailing),
             bottomAnchor.constraint(equalTo: superview.bottomAnchor, constant: -insets.bottom)
