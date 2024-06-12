8d5245877dcaaa95644c7369b02da2e520cc4747
Elvis <elvisnunez@me.com>
2024-06-13 01:43:17 +0200

    Update platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Pinwheel",
+    platforms: [
+        .iOS(.v16)
+    ],
     products: [
         .library(
             name: "Pinwheel",
